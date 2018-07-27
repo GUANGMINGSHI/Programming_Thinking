273bf567063608467840b04f312a7f845ab41d9c
Jimmy <34049205+GUANGMINGSHI@users.noreply.github.com>
2018-07-27 16:27:17 +0900

    Create Pychram shortcut.py

---
+++ Pychram shortcut.py
@@ -0,0 +1,14 @@
+command-y: delete the row
+command-d: copy and paste the row
+command-c: copy this row
+command-left/right: 光标移到行首/行末
+commang-/: comment out and cancel comment out
+command-f-shift enter: previous
+cmd-j:代码自动补全
+command-b: 跳到声明处
+
+shift-command-up/down: shift code by row
+shift-command-left/right: 选择本行光标前/后的所有内容
+shift-enter: 在行中跳到下一行
+
+
